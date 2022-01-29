cf2c5154f60ef82f77ff3b0fcf486bb490eace3c
Shikhar Dhyani <shikhardhyani025@gmail.com>
2022-01-29 23:19:48 +0530

    Update targetSum.cpp

--- 0-1KNAPSACK Problems/targetSum.cpp
+++ 0-1KNAPSACK Problems/targetSum.cpp
@@ -1,41 +1,39 @@
-  #include <bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-  int findTargetSumWays(vector<int>& nums, int S)
-    {
-        int n = nums.size();
-        long int sum = accumulate(nums.begin(), nums.end(), 0);
-        long int s1 = (sum+S)/2;
-        if(S > sum || (sum+S)%2 != 0)
-            return 0;
-        
-        int dp[n+1][s1+1];
-        int zcount = 0;
-        dp[0][0] = 1;
-        for(int i = 1; i < n+1; i++)
+int countsubsetsum(int arr[], int sum, int n)
+{
+    int t[n+1][sum+1];
+    for (int i = 0; i < n + 1; i++)
+        for (int j = 0; j < sum + 1; j++)
         {
-            if(nums[i-1] == 0)
-                zcount++;
-            dp[i][0] = pow(2, zcount);
+            if (i == 0)
+                t[i][j] = 0;
+            if (j == 0)
+                t[i][j] = 1;
         }
-        for(int i = 1; i < s1+1; i++)
-            dp[0][i] = 0;
+    for (int i = 1; i < n + 1; i++)
+        for (int j = 1; j < sum + 1; j++)
+        {
+            if (arr[i - 1] <= j)
+                t[i][j] = t[i - 1][j -arr[i - 1]]+t[i - 1][j];
+            else
+                t[i][j] = t[i - 1][j];
+        }
+    return t[n][sum];
+}
 
-        for(int i = 1; i < n+1; i++)
-            for(int j = 1; j < s1+1; j++)
-            {
-                if(nums[i-1] <= j)
-                    dp[i][j] = dp[i-1][j-nums[i-1]] + dp[i-1][j];
-                else
-                    dp[i][j] = dp[i-1][j];
-            }
-        return dp[n][s1];
-    }
-    int main()
+int main()
 {
 
-    vector<int>v = {6, 2, 3, 1, 12};
-    int n = v.size();
-    cout <<findTargetSumWays(v,12);
+    int arr[] = {0,1,2,10,9,8};
+    int targetSum = 8;
+    int sum=0;
+    
+    int n = sizeof(arr) / sizeof(arr[0]);
+    for(int i=0;i<n;i++)
+    sum+=arr[i];
+    sum=(sum+targetSum)/2;
+    cout << countsubsetsum(arr, sum, n);
     return 0;
 }
